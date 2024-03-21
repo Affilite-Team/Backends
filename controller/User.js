65dbd6a35c3f7a49f6ae7d0ed148200e5da9270b
Abraham  Jude <abraham.jude@netapps.com.ng>
2024-03-21 16:25:00 +0100

    added Try catch to register controller

--- controller/User.js
+++ controller/User.js
@@ -3,25 +3,33 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 exports.signup = async (req, res) => {
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  const User = new userModel({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    userName: req.body.userName,
-    referalId: req.body.referalId,
-    email: req.body.email,
-    password: hashedPassword,
-  });
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    const User = new userModel({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      userName: req.body.userName,
+      referalId: req.body.referalId,
+      email: req.body.email,
+      password: hashedPassword,
+    });
 
-  if (!User) {
-    return res.json({ msg: "invalid fields" });
+    if (!User) {
+      return res.json({ msg: "invalid fields" });
+    }
+    const newUser = await User.save();
+    res.json({
+      msg: "success",
+      code: 200,
+      // data: newUser,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      status: error.status,
+      message: error.message,
+      data: [],
+    });
   }
-  const newUser = await User.save();
-  res.json({
-    msg: "success",
-    code: 200,
-    data: newUser,
-  });
 };
 
 exports.getUsers = async (req, res) => {
